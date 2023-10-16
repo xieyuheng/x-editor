5fb91d15d1c7dfc6dac25e924e4ba7b78d9184f2
Xie Yuheng <xyheme@gmail.com>
2023-10-17 03:50:05 +0800

    EditorModeline -- show mediaType

--- TODO.md
+++ TODO.md
@@ -1,4 +1,3 @@
-EditorModeline -- show mediaType
 EditorTabAudio
 EditorTabVideo
 EditorTabBinary
--- src/pages/editor/EditorModeline.vue
+++ src/pages/editor/EditorModeline.vue
@@ -2,6 +2,7 @@
 import { State } from './State'
 import { Tab } from './Tab'
 import { tabIsModified } from './tabIsModified'
+import { tabMediaType } from './tabMediaType'
 
 defineProps<{
   state: State
@@ -11,23 +12,29 @@ defineProps<{
 
 <template>
   <div
-    class="flex items-baseline space-x-3 overflow-x-auto overflow-y-clip whitespace-pre bg-stone-200 px-3 dark:bg-stone-600"
+    class="flex items-baseline space-x-2 overflow-x-auto overflow-y-clip whitespace-pre bg-stone-200 px-3 dark:bg-stone-600"
   >
     <div class="font-bold">{{ tab.node?.relativePath || tab.file.name }}</div>
+
+    <span class="font-bold">({{ tabMediaType(tab) }})</span>
+
     <div>
       <span>{ </span>
-      <span>line: </span>
-      <span class="font-bold">{{ tab.text.split('\n').length }}</span>
 
-      <span>, </span>
-      <span>length: </span>
-      <span class="font-bold">{{ tab.text.length }}</span>
+      <template v-if="tabMediaType(tab).startsWith('text/')">
+        <span>line: </span>
+        <span class="font-bold">{{ tab.text.split('\n').length }}</span>
+        <span>, </span>
+      </template>
 
       <template v-if="tabIsModified(tab)">
-        <span>, </span>
         <span>isModified: </span>
         <span class="font-bold">true</span>
+        <span>, </span>
       </template>
+
+      <span>length: </span>
+      <span class="font-bold">{{ tab.text.length }}</span>
 
       <span> }</span>
     </div>
