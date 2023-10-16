3b00709621b6254047fc95b691ee498b95687b9e
Xie Yuheng <xyheme@gmail.com>
2023-10-16 17:47:59 +0800

    up

--- src/pages/editor/EditorMarkdownPreview.vue
+++ src/pages/editor/EditorMarkdownPreview.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
 import { parseDocument } from '@xieyuheng/x-markdown'
+import { computed } from 'vue'
 import MarkdownPreview from '../../components/markdown-preview/MarkdownPreview.vue'
 import { State } from './State'
 import { Tab } from './Tab'
-import { stateDocumentInlineImages } from './stateDocumentInlineImages'
 
 const props = defineProps<{
   state: State
   tab: Tab
 }>()
 
-const document = parseDocument(props.tab.text)
+const document = computed(() => parseDocument(props.tab.text))
 
 // The following is an async function.
-stateDocumentInlineImages(props.state, document)
+// stateDocumentInlineImages(props.state, document)
 </script>
 
 <template>
