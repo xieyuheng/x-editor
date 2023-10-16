2d8b237b9816415d3f105655f5dad40216deb03e
Xie Yuheng <xyheme@gmail.com>
2023-10-17 04:02:50 +0800

    open README.md by default

--- src/pages/editor/stateDirectoryOpen.ts
+++ src/pages/editor/stateDirectoryOpen.ts
@@ -1,5 +1,6 @@
 import { State } from './State'
 import { ensurePermission } from './ensurePermission'
+import { stateWorkspaceNodeFileOpen } from './stateWorkspaceNodeFileOpen'
 import { workspaceLoad } from './workspaceLoad'
 
 export async function stateDirectoryOpen(state: State): Promise<void> {
@@ -17,4 +18,13 @@ export async function stateOpenDirectoryHandle(
 
   state.currentWorkspace = await workspaceLoad(directoryHandle)
   state.recentlyOpened.directoryHandles[directoryHandle.name] = directoryHandle
+
+  for (const node of state.currentWorkspace.root.children) {
+    if (
+      node.kind === 'WorkspaceNodeFile' &&
+      node.relativePath === 'README.md'
+    ) {
+      stateWorkspaceNodeFileOpen(state, node)
+    }
+  }
 }
