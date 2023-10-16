248f4288a5ecfe4d4e5571f6ccc6fdd4150d2afd
Xie Yuheng <xyheme@gmail.com>
2023-10-16 17:42:26 +0800

    up

--- TODO.md
+++ TODO.md
@@ -1,3 +1,2 @@
 stateDocumentInlineImages
-
 workspaceFindFile
--- src/pages/editor/stateDocumentInlineImages.ts
+++ src/pages/editor/stateDocumentInlineImages.ts
@@ -1,9 +1,13 @@
-import { Document } from '@xieyuheng/x-markdown'
+import { Document, traverseDocument } from '@xieyuheng/x-markdown'
 import { State } from './State'
 
 export async function stateDocumentInlineImages(
   state: State,
   document: Document,
 ): Promise<void> {
-  //
+  for (const node of traverseDocument(document)) {
+    if (node.kind === 'Image') {
+      console.log(node.src)
+    }
+  }
 }
