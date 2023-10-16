694fb96d0adad1f375a907d5a9b2ad0be2497215
Xie Yuheng <xyheme@gmail.com>
2023-10-16 16:13:01 +0800

    MarkdownPreview -- iframe loading

--- src/components/markdown-preview/MarkdownPreview.vue
+++ src/components/markdown-preview/MarkdownPreview.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { parseDocument } from '@xieyuheng/x-markdown'
 import { ref, watchEffect } from 'vue'
+import Lang from '../../components/lang/Lang.vue'
 
 const props = defineProps<{
   text?: string
 }>()
 
+const iframeLoaded = ref(false)
 const iframeElement = ref<HTMLIFrameElement | undefined>(undefined)
 
 window.addEventListener('message', (event) => {
@@ -32,6 +34,21 @@ function sendDocument() {
 </script>
 
 <template>
-  <iframe ref="iframeElement" src="https://readonly.link/embedded-article">
-  </iframe>
+  <div class="relative flex h-full w-full">
+    <div
+      v-if="!iframeLoaded"
+      class="absolute left-0 top-0 h-full w-full bg-white p-3 text-stone-500 dark:bg-black dark:text-stone-400"
+    >
+      <Lang>
+        <template #zh>iframe 加载中。。。</template>
+        <template #en>Loading iframe...</template>
+      </Lang>
+    </div>
+    <iframe
+      class="h-full w-full"
+      ref="iframeElement"
+      src="https://readonly.link/embedded-article"
+      @load="iframeLoaded = true"
+    ></iframe>
+  </div>
 </template>
